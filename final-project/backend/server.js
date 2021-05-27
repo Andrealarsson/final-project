662cb7968502b7f340e79d8a78b07e947cf112a9
Andrea Larsson <andrea.e.larsson@hotmail.se>
2021-05-27 11:46:33 +0200

    created app.post/signup and app.post/signin in backend.

--- final-project/backend/server.js
+++ final-project/backend/server.js
@@ -1,11 +1,28 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String, 
+    requered: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -16,13 +33,51 @@ const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const salt = bcrypt.genSaltSync()
+    const newUser = await new User({
+      username,
+      password:bcrypt.hashSync(password,salt)
+    }).save()
+    res.json({
+      success: true,
+      userID: newUser._id,
+      username: newUser.username,
+      accesToken: newUser.accessToken
+    })
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Invalig request', error })
+  }
+})
+
+app.post('/signin', async (req, res) => {
+  const { username, password } = req.body
+  try {
+    const user = await User.findOne({ username })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        success: true,
+        userID: user._id,
+        username: user.username,
+        accesToken: user.accessToken
+      })
+    } else {
+      res.status(404).json({ success: false, message: 'User nog found' })
+    }
+  } catch (error) {
+  res.status(400).json({ success: false, message: 'Invalid request', error })
+  }
+})
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
